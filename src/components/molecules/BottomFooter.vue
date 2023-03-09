165766e25b6a74e08390cc0d7f5814766a73da1c
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2023-03-08 22:19:13 -0300

    bottom footer

---
+++ src/components/molecules/BottomFooter.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+<div class="flex flex-col items-center bg-dark-blue-950 text-xs pt-8 lg:pt-2">
+    <div class="flex flex-col lg:flex-row mb-10">
+        <ul class="flex">
+            <li class="px-2 border-none lg:border-r text-center lg:text-left border-gray-600 hover:underline">
+                <a href="#">Condições de uso</a>
+            </li>
+            <li class="px-2 border-none lg:border-r text-center lg:text-left border-gray-600 hover:underline">
+                <a href="#">Notificação de privacidade</a>
+            </li>
+            <li class="px-2 border-none lg:border-r text-center lg:text-left border-gray-600 hover:underline">
+                <a href="#">Cookies</a>
+            </li>
+            <li class="px-2 border-none lg:border-r text-center lg:text-left border-gray-600 hover:underline">
+                <a href="#">Anúncios baseados em interesses</a>
+            </li>
+        </ul>
+        <span class="text-center lg:text-left mt-8 lg:mt-0 ml-4 text-gray-600">
+            &copy; Amazon.com, Inc. ou suas afiliadas
+        </span>
+    </div>
+    <p class="hidden lg:block mb-10 text-gray-600">Amazon serviços de varejo Ltda. | CNPJ: 15.436.940/0001-03</p>
+    <p class="hidden lg:block mb-10 text-gray-600">
+        Av. Juscelino Kubischeck, 2041, torre E, 18º andar - São Paulo
+        <a href="#" class="text-blue-800">Fale conosco</a>
+    </p>
+    <p class="hidden lg:block mb-10 text-gray-600">Formas de pagamento aceitas: cartões de crédito (Visa, Mastercard, Elo, American Express) e boleto.</p>
+</div>
+</template>
+
+<style scoped></style>
--- src/components/organisms/Footer.vue
+++ src/components/organisms/Footer.vue
@@ -1,12 +1,14 @@
 <script setup>
 import TopFooter from "../molecules/TopFooter.vue";
 import MidFooter from "../molecules/MidFooter.vue";
+import BottomFooter from "../molecules/BottomFooter.vue";
 </script>
 
 <template>
   <footer class="flex flex-col text-white">
     <TopFooter />
     <MidFooter />
+    <BottomFooter />
   </footer>
 </template>
 
